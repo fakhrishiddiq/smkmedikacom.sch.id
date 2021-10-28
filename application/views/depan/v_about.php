5e5ed01a4fdb2c8cf561886d9cb3b643bc35033e
fakhrishiddiq <phoenixgamingid@gmail.com>
2021-10-29 03:53:59 +0700

    first commit

---
+++ application/views/depan/v_about.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+    <!--============================= HEADER =============================-->
+    <div class="header-topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-9">
+                    <div class="header-top_address">
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>022-7314090 - 7309811
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>info@medikacom.sch.id
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Jl.Rancabolang, Manjahlega, Jawa Barat 40286
+                        </div>
+                    </div>
+                    <div class="header-top_login2">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="header-top_login mr-sm-3">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2"><img class="img img-responsive" width="200px;" src="<?php echo base_url().'theme/images/logo-dark.png'?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('siswa');?>">Siswa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('blog');?>">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                                </li>
+                          </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+      <section>
+</section>
+<!--//END HEADER -->
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Kata Sambutan</h2>
+                <p class="description" style="text-align: justify;"><img style="float: left; margin: 15px; width:250px;  height:250px" src="<?php echo base_url().'theme/images/welcome-img.jpg'?>" class="img-fluid" alt="#"> Selaras dengan amanat undang-undang Nomor 20 Tahun 2003 tentang Sistem Pendidikan Nasional, bahwa sistem pendidikan nasional harus mampu menjamin pemerataan kesempatan pendidikan dan peningkatan mutu serta relevansi pendidikan untuk menghadapi tantangan perubahan kehidupan lokal, nasional dan global ; sementara undang-undang Nomor 2 pasal 47 tahun 1989 yang menyebutkan bahwa "masyarakat sebagai mitra Pemerintah berkesempatan yang seluas-luasnya untuk berperan serta dalam penyelenggaraan pendidikan nasional". Menyadari hal tersebut maka kami sebagai bagian dari masyarakat Indonesia,melalui Visi Yayasan Pendidikan MEGABRAIN Indonesia ikut berperan serta untuk mewujudkan pendidikan yang dititik beratkan pada pendidikan kejuruan/pendidikan terapan dari tingkatpendidikan menengah hingga pendidikan tinggi.
+                </p>
+                <p style="text-align: justify;">Sebuah sekolah vokasi unggulan yang menyajikan warna dan nuansa yang berbeda sebagai sebuah model sekolah masa depan dengan paradigma baru “pendidikan yang berorientasi kepada manfaat (BENEFIT ORIENTATION)” ; berbasis kompetensi keahlian terapan yang berkualitas tinggi sesuai tuntutan kualifikasi dunia usaha dan industri ; Production Based Education : Kurikulum dirancang/didisain dengan melibatkan dunia usaha dan industri yang relevan dengan program keahlian sehingga “lulusannya langsung kerja.” ; Entrepreneurial thinking adalah juga merupakan bagian dalam proses pendidikan dan pelatihan di SMK Medikacom agar lulusannya mampu berwirausaha sesuai kompetensinya (promoting excellent diploma which satisfy industry qualification as well as having an entrepreneurship minset).
+                </p>
+                <p>Memperhatikan peluang kebutuhan akan SDM dunia usaha dan industri, SMK MedikaCom telah membuka dan mengembangkan berbagai program keahlian yang juga telah terakreditasi “A” antara lain :
+                </p>
+                <p>
+                -Farmasi </br>
+                -Rekayasa Perangkat Lunak </br>
+                -Teknik Komputer Jaringan </br>
+                -Teknik Kendaraan Ringan </br>
+                -Teknik Sepeda Motor </br>
+                -Akuntasi </br>
+                -Teknik Pemesinanan </br>
+                -Tata Boga </br>
+                -Bisnis Retail </br>
+                -Unit Perjalanan Wisata </br>
+                -Manajemen Logistik </br>
+                </p>
+                <p style="text-align: justify;">Maka dengan terbitnya website SMK medikacom bandung ini, merupakan satu bentuk keinginan dan kepedulian kami untuk terus memberikan pelayanan pendidikan yang optimal kepada Masyarakat Luas. Utamanya kepada siswa dan orangtua , alumni, Dunia usaha dan industri dan para stage holder pendidikan laninnya.
+                </p>
+                <p style="text-align: justify;">Semoga fitur-fitur yang kami sediakan dapat memenuhi harapan kita. Dalam kerangka menciptakan sumber daya manusia yang memiliki kompetensi berkualitas tinggi, berakhlak mulia dalam memasuki era globalisasi yang bersyarat kompetisi ini.
+                </p>
+                <p style="text-align: justify;">Mengakhiri sambutan kami ini saya mengutip pepatah dari negeri china : “JIKA INGIN MENGHASILKAN KEMAKMURAN SATU TAHUN TANAMLAH GANDUM DAN JIKA ANDA INGIN MENGHASILKAN KEMAKMURAN SEPULUH TAHUN TANAMLAH POHON NAMUN JIKA ANDA INGIN MENGHASILKAN KEMAKMURAN SERATUS TAHUN KEMBANGKANLAH MANUSIA”
+                </p>
+                <p style="text-align: justify;">Majulah Pendidikan indonesiaku Tercinta..... !</p>
+                <p>‎ ‎      ‎    ‎    </p>
+                <p>Kepala SMK Medikacom</p>
+                <p>Dr. Dovmen Lubis, SE, MM</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= TESTIMONIAL =============================-->
+    <section class="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Testimonial</h2>
+                </div>
+                <div class="col-md-12">
+                    <div class="single-item">
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">SMK Medikacom Bandung benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan SMK Medikacom Bandung dan menjadi siswa terbaik tahun 2018.</p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-1.png'?>" class="img-fluid" alt="#">
+                                <p><span>Hernandez Alvaro</span>Siswa Terbaik 2018</p>
+                            </div>
+                        </div>
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">SMK Medikacom Bandung benar-benar mengagumkan. Saya sangat senang bisa bergabung dengan SMK Medikacom Bandung dan menjadi siswa terbaik tahun 2017. </p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-2.png'?>" class="img-fluid" alt="#">
+                                <p><span>Elanoar Rigby</span>Siswa Terbaik 2017</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END TESTIMONIAL -->
+    <!--============================= DETAILED CHART =============================-->
+    <div class="detailed_chart">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_1.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_guru;?></span> Guru
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_2.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_siswa;?></span> Siswa
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_3.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_files;?></span> Download
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_4.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_agenda;?></span> Agenda</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--//END DETAILED CHART -->
+
+        <!--============================= FOOTER =============================-->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="foot-logo">
+                            <a href="<?php echo site_url();?>">
+                                <img src="<?php echo base_url().'theme/images/logo-white.png'?>" class="img-fluid" alt="footer_logo">
+                            </a>
+                            <p><?php echo date('Y');?> © copyright by <a href="http://mfikri.com" target="_blank">M Fikri</a>. <br>All rights reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="sitemap">
+                                <h3>Menu Utama</h3>
+                                <ul>
+                                    <li><a href="<?php echo site_url();?>">Home</a></li>
+                                    <li><a href="<?php echo site_url('about');?>">About</a></li>
+                                    <li><a href="<?php echo site_url('artikel');?>">Blog </a></li>
+                                    <li><a href="<?php echo site_url('galeri');?>">Gallery</a></li>
+                                    <li><a href="<?php echo site_url('contact');?>">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                          <div class="sitemap">
+                              <h3>Akademik</h3>
+                              <ul>
+                                  <li><a href="<?php echo site_url('guru');?>">Guru</a></li>
+                                  <li><a href="<?php echo site_url('siswa');?>">Siswa </a></li>
+                                  <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
+                                  <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
+                                  <li><a href="<?php echo site_url('download');?>">Download</a></li>
+                              </ul>
+                          </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="address">
+                                <h3>Hubungi Kami</h3>
+                                <p><span>Alamat: </span> Jl.Rancabolang, Manjahlega, Jawa Barat 40286</p>
+                                <p>Email : info@medikacom.sch.id
+                                    <br> Phone : 022-7314090 - 7309811, Fax : 022-7314088</p>
+                                    <ul class="footer-social-icons">
+                                        <li><a href="#"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!--//END FOOTER -->
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+                <!-- Plugins -->
+                <script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+                <!-- Subscribe -->
+                <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+                <!-- Script JS -->
+                <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+            </body>
+
+            </html>
